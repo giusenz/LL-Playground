5b719f9be70bb4b3b432f4eb117a0e067ddd519f
Giuseppe Nozza <124568437+giusenz@users.noreply.github.com>
2026-01-12 10:46:47 +0100

    Add files via upload

---
+++ cp.c
@@ -0,0 +1,56 @@
+/* fopen, fread and fwrite are not used on purpose */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int cp(char *src, char *dest) {    
+    /* Open files. Destination file is created if needed */
+    int ret = 0;
+
+    int srcd = open(src,O_RDONLY);
+    if (srcd == -1) {
+        perror("Unable to open source file");
+        return 1;
+    }     
+    int destd = open(dest,O_WRONLY|O_CREAT|O_TRUNC,0644);
+    if (destd == -1) {
+        perror("Unable to open destination file");
+        ret = 1;
+        goto close_source;
+    }  
+    
+    /* Read and copy */
+    char buffer[4096];
+    ssize_t nread;
+    while (1) {
+        nread = read(srcd,buffer,sizeof(buffer));
+        if (nread == -1) {
+            perror("Unable to read from source file");
+            ret = 1;
+            goto close_both;
+        }
+        if (nread == 0) break;
+        ssize_t written = write(destd,buffer,nread);
+        if (written == -1) {
+            perror("Unable to write on destination file");
+            ret = 1;
+            goto close_both;
+        }
+    }
+    /* Close files */
+    close_both:
+        close(destd);
+    close_source:
+        close(srcd);
+    
+    return ret;
+}
+
+int main(int argc, char **argv) { 
+    if (argc != 3) {
+        fprintf(stderr,"./cp [SOURCE FILE PATH] [DESTINATION FILE PATH]");
+        return 1;
+    }
+    return cp(argv[1],argv[2]);
+}
