a141518e71678a0e15aa9064da41c81c353e4c60
Giuseppe Nozza <124568437+giusenz@users.noreply.github.com>
2026-01-12 11:46:06 +0100

    Update mycat.h

--- mycat/mycat.h
+++ mycat/mycat.h
@@ -1,15 +1,21 @@
 #ifndef MYCAT_H
 #define MYCAT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /* Given a message, this function prints it on stderr */
 void end(const char *msg);
 
 /* Given a file name, this function reads 
  * the content of the corresponding file and prints it on stdout */
-void read(char *filename);
+void myread(char *filename);
+
+/* No arguments behaviour */
+int toscreen(void);
 
 #endif
