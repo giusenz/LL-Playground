1277de2a3e2541450e3723e3d1011aa82f1993d5
Giuseppe Nozza <124568437+giusenz@users.noreply.github.com>
2026-01-12 11:45:48 +0100

    Refactor mycat.c for better error handling
    Refactor mycat.c to improve error handling and add toscreen function.

--- mycat/mycat.c
+++ mycat/mycat.c
@@ -1,32 +1,46 @@
-#include "mycat.h"
+    #include "mycat.h"
 
-void end(const char *msg) {
-   perror(msg);
-   exit(1); 
-}
-
-void read(char *filename) {
-    FILE *f = fopen(filename,"rb");
-    if (f == NULL) end("File opening failed\n");
-    
-   
-    char buffer[BUFSIZ];   
-    size_t bytes_read;
-
-    while ((bytes_read = fread(buffer,1,sizeof(buffer),f)) > 0) {
-        fwrite(buffer,1,bytes_read,stdout);    
-    }  
-        
-    if (ferror(f)) {
-        end("File read error");
+    void end(const char *msg) 
+    {
+    fprintf(stderr,msg);
+    puts("");
+    exit(1); 
     }
- 
-    fclose(f);
-}
 
+    void myread(char *filename) {
+        FILE *f = fopen(filename,"rb");
+        if (f == NULL) end("File opening failed\n");
+        
+    
+        char buffer[BUFSIZ];   
+        size_t bytes_read;
 
-int main(int argc, char **argv) {
-    if (argc != 2) end("Usage: ./mycat [FILE]");
-    read(argv[1]);
-    return 0;
-}
+        while ((bytes_read = fread(buffer,1,sizeof(buffer),f)) > 0) {
+            fwrite(buffer,1,bytes_read,stdout);    
+        }  
+            
+        if (ferror(f)) {
+            end("File read error");
+        }
+    
+        fclose(f);
+    }
+
+    int toscreen(void) {
+        ssize_t nread;
+        char buffer[4096];
+        while (true) {
+            nread = read(0,buffer,sizeof(buffer));
+            if (nread == -1) return -1;
+            if (nread == 0) break;
+            write(1,buffer,nread);
+        }
+        return 0;
+    }
+
+    int main(int argc, char **argv) {
+        if (argc > 2) end("Usage: ./mycat [FILE] or ./mycat");
+        if (argc == 1) toscreen();
+        else myread(argv[1]);
+        return 0;
+    }
